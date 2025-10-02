69ca8d95f2dfd6fcd6f55bf3ddf3918db368e2c0
Alon Barnea <alon.barnea@cajarobotics.com>
2025-10-03 02:32:38 +0300

    Change paths to relative

--- sw.js
+++ sw.js
@@ -1,34 +1,18 @@
 const CACHE_NAME = 'timer-app-v2';
 const urlsToCache = [
-  '/',              // important if start_url is "/"
-  '/index.html',
-  '/script.js',
-  '/styles.css',
-  '/manifest.json',
-  '/icon.png',
-  '/icon-512.png'
+  './',
+  './index.html',
+  './script.js',
+  './styles.css',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
 ];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(urlsToCache)));
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
-    )
-  );
-  self.clients.claim();
 });
 
 self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request).then(res => {
-      const copy = res.clone();
-      caches.open(CACHE_NAME).then(c => c.put(e.request, copy)).catch(()=>{});
-      return res;
-    }).catch(() => caches.match('/index.html')))
-  );
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
 });
